dd045fd044ffcf25bdfc4838b4413cbbaf71c2fe
Razielini <razielco@gmail.com>
2021-05-28 11:15:21 -0600

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -5,9 +5,9 @@ import tags from './tags';
 export default {
   clusterName: process.env.CLUSTER_NAME || 'mobileMicroservices',
   parameters: {
-    repositoryName: process.env.REPOSITORY_NAME || '/cdk-iot-pipeline/dev/respository_name',
-    repositoryOwner: process.env.REPOSITORY_OWNER || '/cdk-iot-pipeline/dev/respository_owner',
-    repositoryBranch: process.env.REPOSITORY_BRANCH || '/cdk-iot-pipeline/dev/respository_branch',
+    repositoryName: process.env.REPOSITORY_NAME || '/iac-mobile-stack/dev/respository_name',
+    repositoryOwner: process.env.REPOSITORY_OWNER || '/iac-mobile-stack/dev/respository_owner',
+    repositoryBranch: process.env.REPOSITORY_BRANCH || '/iac-mobile-stack/dev/respository_branch',
   },
   repositories,
   tags
